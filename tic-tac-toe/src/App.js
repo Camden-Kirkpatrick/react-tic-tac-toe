7016a3121d1e4700dea2c6e78727ec8b44adb652
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-05-16 17:54:15 -0500

    refactored code and moved state to Game

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -9,11 +9,8 @@ function Square({ value, onSquareClick })
   );
 }
 
-export default function Board()
+function Board({ xIsNext, squares, onPlay })
 {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
-
   function handleClick(i)
   {
     // Prevent overwriting a space that is already filled
@@ -27,8 +24,7 @@ export default function Board()
     else
       nextSquares[i] = "O";
 
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -60,6 +56,30 @@ export default function Board()
       </div>
     </>
   );
+}
+
+export default function Game()
+{
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares)
+  {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol></ol>
+      </div>
+    </div>
+  )
 }
 
 function calculateWinner(squares)
